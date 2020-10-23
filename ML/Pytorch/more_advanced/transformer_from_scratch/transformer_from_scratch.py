71faea865041410473a8c0543aaf48b51c63e92f
Bilgin Aksoy <bilginaksoy2003@yahoo.com>
2020-10-23 16:49:12 +0300

    Update transformer_from_scratch.py

--- ML/Pytorch/more_advanced/transformer_from_scratch/transformer_from_scratch.py
+++ ML/Pytorch/more_advanced/transformer_from_scratch/transformer_from_scratch.py
@@ -284,7 +284,7 @@ if __name__ == "__main__":
     trg_pad_idx = 0
     src_vocab_size = 10
     trg_vocab_size = 10
-    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx).to(
+    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, device=device).to(
         device
     )
     out = model(x, trg[:, :-1])
