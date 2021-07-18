afc5b4c651bcbb4a95a186fb338beca025ccf00b
Aryaman <58369369+aryaman4152@users.noreply.github.com>
2021-07-18 22:59:33 +1000

    Update pytorch_resnet.py

--- ML/Pytorch/CNN_architectures/pytorch_resnet.py
+++ ML/Pytorch/CNN_architectures/pytorch_resnet.py
@@ -108,7 +108,7 @@ class ResNet(nn.Module):
         x = self.avgpool(x)
         x = x.reshape(x.shape[0], -1)
         x = self.fc(x)
-        x = self.softmax(1)
+        x = self.softmax(x)
         return x
 
     def _make_layer(self, block, num_residual_blocks, intermediate_channels, stride):
