f96828fb4b446de60f50700119ee0b3d4dc2ee8a
nikiopta <72725100+nikiopta@users.noreply.github.com>
2023-04-23 19:36:22 +0300

    Update tutorial4-convnet.py

--- ML/TensorFlow/Basics/tutorial4-convnet.py
+++ ML/TensorFlow/Basics/tutorial4-convnet.py
@@ -10,7 +10,7 @@ from tensorflow.keras.datasets import cifar10
 physical_devices = tf.config.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0
 
