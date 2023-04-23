b38bc6fe5e2a7a79f8d7d649c2f081cdb8aadcca
nikiopta <72725100+nikiopta@users.noreply.github.com>
2023-04-23 19:44:27 +0300

    Update tutorial15-customizing-modelfit.py

--- ML/TensorFlow/Basics/tutorial15-customizing-modelfit.py
+++ ML/TensorFlow/Basics/tutorial15-customizing-modelfit.py
@@ -5,9 +5,9 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.datasets import mnist
+tf.keras.datasets.mnist.load_data(path="mnist.npz")
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype("float32") / 255.0
 
